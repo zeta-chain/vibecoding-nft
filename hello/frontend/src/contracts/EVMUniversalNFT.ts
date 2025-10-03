4341258c37cc1e12dd68d08bc85598e829c952b7
Hernan Clich <hernan.g.clich@gmail.com>
2025-10-03 12:37:26 -0300

    Implemented safeMint

---
+++ hello/frontend/src/contracts/EVMUniversalNFT.ts
@@ -0,0 +1,86 @@
+import { ethers } from 'ethers';
+
+// ABI for the EVMUniversalNFT contract
+export const EVMUniversalNFT_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "uri",
+        "type": "string"
+      }
+    ],
+    "name": "safeMint",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "pause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "unpause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const;
+
+export const createNFTContract = (
+  address: string,
+  signer: ethers.AbstractSigner
+): ethers.Contract => {
+  return new ethers.Contract(
+    address,
+    EVMUniversalNFT_ABI,
+    signer
+  );
+};
